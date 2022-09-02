07df2a7a1938c1cd051aedc44f8f7b29a5fc9156
premiumdas <ezioaudi777@gmail.com>
2022-09-03 00:54:33 +0530

    Topfold left

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div>
+    <div className='max-width'>
       <Header />
       <TopFold />
       <BrandInt />
--- src/components/TopFold/TopFold.js
+++ src/components/TopFold/TopFold.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Button from '../../common/Button/Button';
 import "./TopFold.css";
 
 const TopFold = () => {
   return (
-    <div>TopFold</div>
-  )
-}
+    <div className='topfold absolute-center'>
+      <div className='tf-left'>
+        <div className='tf-heading'>
+          Discover, collect & sell{' '} 
+          <span className='header-gradient'>Extraordinary </span>
+          NFTs
+        </div>
+        <div className='tf-description'>
+          some description abt nfts blah blah blah
+        </div>
+        <div className='tf-left-btns'>
+            <Button btnType="PRIMARY" btnText="EXPLORE" />
+            <Button btnType="SECONDARY" btnText="CREATE" customClass='tf-left-secondary-btn'/>
+        </div>
+        <div className='tf-left-infoStats'>
+          <div className='tf-left-infoItem absolute-center'>
+            <div className='tf-infoItem-count'>180K+</div>
+            <div className='tf-infoItem-label'>Collections</div>
+          </div>
+          <div className='tf-left-infoItem absolute-center'>
+            <div className='tf-infoItem-count'>19K+</div>
+            <div className='tf-infoItem-label'>Artists</div>
+          </div>
+          <div className='tf-left-infoItem absolute-center'>
+            <div className='tf-infoItem-count'>420K+</div>
+            <div className='tf-infoItem-label'>Community</div>
+          </div>
+        </div>
+      </div>
+      <div className='tf-right'></div>
+    </div>
+  );
+};
 
 export default TopFold
